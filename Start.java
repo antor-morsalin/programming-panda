670930aac90a55a70a89e3dbaaabc4fcf3783c6b
Mahmudul Morsalin Antor <142907506+antor-morsalin@users.noreply.github.com>
2023-08-22 21:21:36 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,27 @@
+public class Start 
+{
+    public static void main (String [] ProgrammingPanda)
+    {
+        new StartPage();
+        //new SignIn();
+        //new LogIn();
+        //new HomePage();
+        //new Exit();
+        //new UserInfo();
+        //new Settings();
+        //new Policy();
+        //new UpdateUserInfo();
+        //new setGuardianInfo();
+        //new UpdateGuardianInfo();
+        //new Tutorial();
+        //new Beginner();
+        //new StartCoding();
+        //new adminEntry();
+        //new Admin();
+        //new print_tutorial();
+        //new var_tutorial();
+        //new cal_tutorial();
+        //new if_tutorial();
+
+    }
+}
